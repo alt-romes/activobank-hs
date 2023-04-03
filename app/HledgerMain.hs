1ce915ccba6af6f620d6fa826965357b3e02eeec
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2023-04-03 14:23:08 +0100

    Rules and decimal numbers

--- app/HledgerMain.hs
+++ app/HledgerMain.hs
@@ -1,66 +1,82 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE TypeApplications #-}
 
+import qualified Data.List.NonEmpty as NE
 import Debug.Trace (traceShow)
 import Data.Either
+import Data.Decimal
 import Data.Time.Calendar
 import qualified Data.Text.IO as T
+import qualified Data.Text as T
 import Data.Char
 import Data.Proxy
 import Data.String
 import Control.Monad.IO.Class
+import System.Directory
+import System.FilePath
 
 import Hledger
 
 import ActivoBank
 
+-- | A mapping from a query on transaction to the account associated with the
+-- transaction (on the other end)
+type Rules = [(Query,AccountName)]
+
 --------------------------------------------
 
-scrapeActivoBank :: Journal -> [Int] -> String -> String -> String -> IO ()
-scrapeActivoBank journal codes user fingerprint browserI = do
+scrapeActivoBank :: Journal -> Rules -> [Int] -> String -> String -> String -> IO ()
+scrapeActivoBank journal rules codes user fingerprint browserI = do
 
   -- Get movements from activo bank
   movements <- withSession codes user fingerprint browserI (fetchMovementsTable 50)
 
   -- Add movements to HLedger if they are new
-  let newTransactions = foldl (insertIfNew journal) [] movements
+  let newTransactions = foldl (insertIfNew journal rules) [] movements
 
   mapM_ (T.putStr . showTransaction) newTransactions
   mapM_ (T.appendFile (journalFilePath journal) . showTransaction) newTransactions
 
 --------------------------------------------
 
-insertIfNew :: Journal -> [Transaction] -> Movement -> [Transaction]
-insertIfNew journal trs mov@(Mov day (fromString -> dd) amt bal) =
+insertIfNew :: Journal -> Rules -> [Transaction] -> Movement -> [Transaction]
+insertIfNew journal rules trs mov@(Mov day (fromString -> dd) amt bal) =
     -- FIXME: If there are two transactions with the same description on the
     -- same day, we will ignore them, but only the second time this is run the
     -- same day. Does it even matter? Only if we delete an entry we previously logged?
-  if null $ filter ((> 0.5) . fst) $
+  if not $ any ((> 0.5) . fst) $
             journalTransactionsSimilarTo journal
               (And [ Desc   (either (\e -> traceShow e (toRegex' ".*")) id $ toRegex dd)
                    , Date $ DateSpan (Just (Exact day)) (Just (Exact (addDays 1 day)))
                    ]) dd 1
-    then (toTransaction mov):trs
+    then toTransaction rules mov:trs
     else trs
 
-toTransaction :: Movement -> Transaction
-toTransaction (Mov day dd (toRational -> amt) bal) =
-  (transaction day
-  [ post "Assets:Checking:ActivoBank" $ Amount "€" (fromRational amt) amountstyle{ascommodityside=R} Nothing
-  , post (if amt > 0 then "Income" else "Expenses") $ Amount "€" (- (fromRational amt)) amountstyle{ascommodityside=R} Nothing
-  ]) { tdescription = fromString dd }
+toTransaction :: Rules -> Movement -> Transaction
+toTransaction rules (Mov day dd (realFracToDecimal 2 -> amt) bal) =
+  (transaction day [abPost, otherPost]) { tdescription = fromString dd }
+    where
+      abPost = post "Assets:Checking:ActivoBank" $ Amount "€" amt amtstyle Nothing
+      tmpTransaction = (transaction day [abPost]) { tdescription = fromString dd }
+      otherPost = post (case NE.nonEmpty (filter ((`matchesTransaction` tmpTransaction) . fst) rules) of
+                          Just ((_,acc) NE.:| _) -> acc
+                          Nothing -> if amt > 0 then "Revenue:TODO" else "Expenses:TODO")
+                    $ Amount "€" (-amt) amtstyle Nothing
+      amtstyle = amountstyle{ascommodityside=R,asprecision=NaturalPrecision}
 
 --------------------------------------------
 
 main :: IO ()
 main = do
+  xdgConfigDir <- getXdgDirectory XdgConfig "activobank"
+
   -- Sessions
-  [map digitToInt -> codes, user, fingerprint, browserI] <- lines <$> readFile "activobank.secret"
+  [map digitToInt -> codes, user, fingerprint, browserI] <- lines <$> readFile (xdgConfigDir </> ".activobank.secret")
+  rules <- map ((\(qstr, acc) -> (either error fst (parseQuery nulldate qstr), T.drop 4 acc)) . T.breakOn "==> ")
+              . T.lines <$> T.readFile (xdgConfigDir </> "rules.txt")            -- Match ^ characters, ==> and the space afterwards
 
   journal <- defaultJournal
 
-  scrapeActivoBank journal codes user fingerprint browserI
+  scrapeActivoBank journal rules codes user fingerprint browserI
+
